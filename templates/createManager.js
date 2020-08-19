0d0aae94803de5b5317bc03b09cb6577eeebda73
L.Walker <logantwalker95@gmail.com>
2020-08-18 21:52:58 -0400

    building js modules to create html cards

---
+++ templates/createManager.js
@@ -0,0 +1,13 @@
+let col = `<div class="col"></div>`;
+let card = `<div class="card blue-grey darken-1"></div>`;
+let cardContent = `<div class="card-content white-text"></div>`;
+let cardTitle = `<span class="card-title">Card Title</span>`
+let cardText = `<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>`;
+
+      
+        
+        
+          
+        
+        
+  
